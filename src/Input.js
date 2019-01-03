5c69765b784e75365e65bfb32cecacacc6f4b285
Terence1991 <cplterencehawes@gmail.com>
2019-01-03 04:49:22 +0000

    input field added

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,14 @@
 import React from 'react'
+// import styled from 'styled-components'
+import './index.css'
 
 const Input = () => {
   return (
     <div className='Input'>
-  <textarea></textarea>
+    <div class="thought-bubble bubble">
+    <textarea></textarea>
+    </div>
+
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import ThoughtBubble from "./ThoughtBubble.js"
 import Title from "./Title.js"
 import Input from "./Input.js"
+import "./index.css"
 
 const App = () => {
   return (
-  <div className='ui container comments'>
+  <div className='bubble'>
       <ThoughtBubble /> 
       <Title/>
       <Input/>
