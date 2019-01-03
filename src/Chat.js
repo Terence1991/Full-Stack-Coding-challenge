c994973b9b66dc8f6ec7d7821fa2cf9a4c1dce08
Terence1991 <cplterencehawes@gmail.com>
2019-01-03 17:00:47 +0000

    chat box finished

--- src/Chat.js
+++ src/Chat.js
@@ -1,57 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
-class ResizableTextarea extends React.PureComponent {
-	constructor(props) {
-		super(props);
-		this.state = {
-			value: '',
-			rows: 5,
-			minRows: 5,
-			maxRows: 10,
-		};
-	}
-	
-	handleChange = (event) => {
-		const textareaLineHeight = 24;
-		const { minRows, maxRows } = this.state;
-		
-		const previousRows = event.target.rows;
-  	event.target.rows = minRows; // reset number of rows in textarea 
-		
-		const currentRows = ~~(event.target.scrollHeight / textareaLineHeight);
-    
-    if (currentRows === previousRows) {
-    	event.target.rows = currentRows;
-    }
-		
-		if (currentRows >= maxRows) {
-			event.target.rows = maxRows;
-			event.target.scrollTop = event.target.scrollHeight;
-		}
-    
-  	this.setState({
-    	value: event.target.value,
-      rows: currentRows < maxRows ? currentRows : maxRows,
-    });
-	};
-	
-	render() {
-		return (
-			<textarea
-				rows={this.state.rows}
-				value={this.state.value}
-				placeholder={'Enter your text here...'}
-				className={'textarea'}
-				onChange={this.handleChange}
-			/>
-		);
-	}
+
+const Chat = () => {
+  return (
+<div className="search-wrapper">
+	<form>
+	<input type="text" name="focus" className="search-box" placeholder="Share your Thoughts" />
+		<button className="close-icon" type="reset"></button>
+	</form>
+</div>
+  )
 }
 
-ReactDOM.render(
-	<ResizableTextarea/>,
-	document.querySelector('#root')
-);
-
-export default ResizableTextarea
+export default Chat;
--- src/Input.js
+++ src/Input.js
@@ -1,15 +1,16 @@
 import React from 'react'
 // import styled from 'styled-components'
 import './index.css'
-import ResizableTextarea from "./Chat.js"
+import Chat from './Chat.js'
 
 const Input = () => {
   return (
     <div className='Input'>
     <div className="thought-bubble bubble">
-    <textarea className='text'>
-    <ResizableTextarea/>
-    </textarea>
+    <span className='title'>
+    <p className='chat'> The Chat box</p>
+    </span>
+    <Chat/>
     </div>
 
     </div>
